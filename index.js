d2a30ca05fddfc952c051631329f982beafbc47b
Forbes Lindesay <forbes@lindesay.co.uk>
2014-07-13 16:30:17 +0100

    Initial code

---
+++ index.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var crypto = require('crypto');
+var zlib = require('zlib');
+var ms = require('ms');
+var mime = require('mime');
+
+module.exports = prepareResponse;
+function prepareResponse(body, headers) {
+  return new PreparedResponse(body, headers);
+}
+function PreparedResponse(body, headers) {
+  if (typeof body === 'string') body = new Buffer(body);
+  if (!Buffer.isBuffer(body)) {
+    throw new TypeError('Text must be either a buffer or a string');
+  }
+  this.body = body;
+  this.gzippedBody = null;
+  this.waiting = [];
+  zlib.gzip(body, function (err, res) {
+    if (err) {
+      console.error(err.stack);
+    } else if (res.length < this.body.length) {
+      this.gzippedBody = res;
+    }
+    var waiting = this.waiting;
+    this.waiting = null;
+    waiting.forEach(function (fn) {
+      fn();
+    });
+  }.bind(this));
+  this.etag = md5(body);
+
+  this.headers = Object.keys(headers || {}).map(function (header) {
+    var value = headers[header];
+    if (header.toLowerCase() === 'cache-control') {
+      if (typeof value === 'string' && ms(value)) {
+        value = 'public, max-age=' + Math.floor(ms(value) / 1000);
+      } else if (typeof headers.cache === 'number') {
+        value = 'public, max-age=' + Math.floor(value / 1000);
+      }
+    }
+    if (header.toLowerCase() === 'content-type' && value.indexOf('/') === -1) {
+      value = mime.lookup(value);
+    }
+    return new Header(header, value);
+  });
+}
+PreparedResponse.prototype.send = function (req, res, next) {
+  if (this.waiting) {
+    return this.waiting.push(this.send.bind(this, req, res, next));
+  }
+
+  this.headers.forEach(function (header) {
+    header.set(res);
+  });
+
+  // vary
+  if (!res.getHeader('Vary')) {
+    res.setHeader('Vary', 'Accept-Encoding');
+  } else if (!~res.getHeader('Vary').indexOf('Accept-Encoding')) {
+    res.setHeader('Vary', res.getHeader('Vary') + ', Accept-Encoding');
+  }
+
+  //check old etag
+  if (req.headers['if-none-match'] === this.etag) {
+    res.statusCode = 304;
+    res.end();
+    return;
+  }
+
+  //add new etag
+  res.setHeader('ETag', this.etag);
+
+  //add gzip
+  if (supportsGzip(req) && this.gzippedBody) {
+    res.setHeader('Content-Encoding', 'gzip');
+    res.setHeader('Content-Length', this.gzippedBody.length);
+    if ('HEAD' === req.method) res.end();
+    else res.end(this.gzippedBody);
+  } else {
+    res.setHeader('Content-Length', this.body.length);
+    if ('HEAD' === req.method) res.end();
+    else res.end(this.body);
+  }
+};
+
+function Header(key, value) {
+  this.key = key;
+  this.value = value;
+}
+Header.prototype.set = function (res) {
+  res.setHeader(this.key, this.value);
+};
+
+function md5(str) {
+  return crypto.createHash('md5').update(str).digest("hex");
+}
+
+function supportsGzip(req) {
+  return req.headers
+      && req.headers['accept-encoding']
+      && req.headers['accept-encoding'].indexOf('gzip') !== -1;
+}
---
+++ test/index.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var fs = require('fs');
+var express = require('express');
+var prepare = require('../');
+
+var app = express();
+
+var HTML_STRING = fs.readFileSync(__dirname + '/test.html', 'utf8');
+var JS_STRING = 'alert("foo");';
+var html = prepare(HTML_STRING, {'content-type': 'html'});
+var script = prepare(JS_STRING, {'content-type': 'js', 'cache-control': '1 year'});
+
+app.get('/', function (req, res, next) {
+  html.send(req, res, next);
+});
+app.get('/client.js', function (req, res, next) {
+  script.send(req, res, next);
+});
+
+var server = app.listen(3000);
+
+var assert = require('assert');
+var http = require('http');
+var zlib = require('zlib');
+var Promise = require('promise');
+var concat = require('concat-stream');
+var test = require('testit');
+
+function request(path, headers) {
+  return new Promise(function (resolve, reject) {
+    http.request({
+      host: 'localhost',
+      port: 3000,
+      path: path,
+      method: 'GET',
+      headers: headers
+    }, function (res) {
+      res.on('error', reject);
+      res.pipe(concat(function (body) {
+        resolve({statusCode: res.statusCode, body: body, headers: res.headers});
+      }));
+    }).on('error', reject).end();
+  });
+}
+function gunzip(body) {
+  return new Promise(function (resolve, reject) {
+    zlib.gunzip(body, function (err, res) {
+      if (err) reject(err);
+      else resolve(res);
+    }.bind(this));
+  });
+}
+test('raw-request', function () {
+  return request('/', {}).then(function (res) {
+    assert(res.statusCode === 200);
+    assert(res.body.toString() === HTML_STRING);
+    assert(res.headers.vary === 'Accept-Encoding');
+    assert(res.headers.etag === '76f7dd7da1deaa8cec3648c1f4790852');
+    assert(res.headers['content-length'] === '16530');
+    assert(res.headers['content-type'] === 'text/html');
+  });
+});
+test('gzip-request', function () {
+  return request('/', {'accept-Encoding': 'gzip'}).then(function (res) {
+    assert(res.statusCode === 200);
+    assert(res.headers.vary === 'Accept-Encoding');
+    assert(res.headers.etag === '76f7dd7da1deaa8cec3648c1f4790852');
+    assert(res.headers['content-length'] === '4525');
+    assert(res.headers['content-type'] === 'text/html');
+    assert(res.headers['content-encoding'] === 'gzip');
+    return gunzip(res.body);
+  }).then(function (body) {
+    assert(body.toString() === HTML_STRING);
+  });
+});
+test('etag-request', function () {
+  return request('/', {'if-None-Match': '76f7dd7da1deaa8cec3648c1f4790852'}).then(function (res) {
+    assert(res.statusCode === 304);
+    assert(res.body.length === 0);
+  });
+});
+
+
+test('raw-request-2', function () {
+  return request('/client.js', {}).then(function (res) {
+    assert(res.statusCode === 200);
+    assert(res.body.toString() === JS_STRING);
+    assert(res.headers.vary === 'Accept-Encoding');
+    assert(res.headers.etag === 'af5c77b360ffe88dbed4b3d71e3d0eba');
+    assert(res.headers['content-length'] === '13');
+    assert(res.headers['content-type'] === 'application/javascript');
+    assert(res.headers['cache-control'] === 'public, max-age=31557600');
+  });
+});
+test('gzip-request-2', function () {
+  // won't actually gzip because the text response is shorter without gzip
+  return request('/client.js', {'accept-Encoding': 'gzip'}).then(function (res) {
+    assert(res.statusCode === 200);
+    assert(res.body.toString() === JS_STRING);
+    assert(res.headers.vary === 'Accept-Encoding');
+    assert(res.headers.etag === 'af5c77b360ffe88dbed4b3d71e3d0eba');
+    assert(res.headers['content-length'] === '13');
+    assert(res.headers['content-type'] === 'application/javascript');
+    assert(res.headers['cache-control'] === 'public, max-age=31557600');
+    assert(res.headers['content-encoding'] !== 'gzip');
+  });
+});
+
+test('cleanup', function () {
+  server.close();
+});
