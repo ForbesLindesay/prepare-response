feedf629c659dd3450db09a232028e9496970a07
Forbes Lindesay <forbes@lindesay.co.uk>
2017-07-10 14:10:05 +0100

    Test without using express

--- test/index.js
+++ test/index.js
@@ -1,24 +1,33 @@
 'use strict';
 
 var fs = require('fs');
-var express = require('express');
+var http = require('http');
 var prepare = require('../');
-
-var app = express();
 
 var HTML_STRING = fs.readFileSync(__dirname + '/test.html', 'utf8');
 var JS_STRING = 'alert("foo");';
 var html = prepare(HTML_STRING, {'content-type': 'html'});
 var script = prepare(JS_STRING, {'content-type': 'js', 'cache-control': '1 year'});
+var scriptNoGzip = prepare(HTML_STRING, {'content-type': 'html'}, {gzip: false});
 
-app.get('/', function (req, res, next) {
-  html.send(req, res, next);
-});
-app.get('/client.js', function (req, res, next) {
-  script.send(req, res, next);
+function next(err) {
+  throw err;
+}
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      html.send(req, res, next);
+      break;
+    case '/client.js':
+      script.send(req, res, next);
+      break;
+    case '/no-gzip':
+      scriptNoGzip.send(req, res, next);
+      break;
+  }
 });
 
-var server = app.listen(3000);
+server.listen(3000);
 
 var assert = require('assert');
 var http = require('http');
@@ -104,6 +113,34 @@ test('gzip-request-2', function () {
     assert(res.headers['content-type'] === 'application/javascript');
     assert(res.headers['cache-control'] === 'public, max-age=31557600');
     assert(res.headers['content-encoding'] !== 'gzip');
+  });
+});
+
+
+test('raw-request gzip=false', function () {
+  return request('/no-gzip', {}).then(function (res) {
+    assert(res.statusCode === 200);
+    assert(res.body.toString() === HTML_STRING);
+    assert(res.headers.vary !== 'Accept-Encoding');
+    assert(res.headers.etag === '76f7dd7da1deaa8cec3648c1f4790852');
+    assert(res.headers['content-length'] === '16530');
+    assert(res.headers['content-type'] === 'text/html');
+  });
+});
+test('gzip-request gzip=false', function () {
+  return request('/no-gzip', {'accept-Encoding': 'gzip'}).then(function (res) {
+    assert(res.statusCode === 200);
+    assert(res.body.toString() === HTML_STRING);
+    assert(res.headers.vary !== 'Accept-Encoding');
+    assert(res.headers.etag === '76f7dd7da1deaa8cec3648c1f4790852');
+    assert(res.headers['content-length'] === '16530');
+    assert(res.headers['content-type'] === 'text/html');
+  });
+});
+test('etag-request gzip=false', function () {
+  return request('/no-gzip', {'if-None-Match': '76f7dd7da1deaa8cec3648c1f4790852'}).then(function (res) {
+    assert(res.statusCode === 304);
+    assert(res.body.length === 0);
   });
 });
 
